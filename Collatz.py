8b0d3be6e9155e11ddfaa8b197788f52d0ad8999
Gabriel Campa <89239208+DurinKhampa@users.noreply.github.com>
2023-02-01 04:54:23 +0000

    Name

---
+++ Collatz.py
@@ -0,0 +1,19 @@
+while True:
+  try:
+    num = int(input('Type a number: '))
+  except ValueError:
+    print("That's not an integer number")
+    num = int(input('Type a number: '))
+  
+    while num != 1:
+      if num != 1:
+        if num % 2 == 0:
+          num = num / 2
+          print(int(num))
+        else:
+          num += 1
+          print(int(num))
+      else:
+        print(int(num))
+      num = num
+  break
